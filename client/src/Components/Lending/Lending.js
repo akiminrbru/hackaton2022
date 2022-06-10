cf9310fa4788ae2b29a4a9bf9c16915cc7170883
ArkoniX21 <chernovsergey21@yandex.ru>
2022-06-10 22:09:59 +0300

    Revert "Revert "Merge branch 'master' of https://github.com/kiskisnotdead/hackaton2022""
    This reverts commit 3885e073a310cead525fa7688f6fe78b89fee7c4.

---
+++ client/src/Components/Lending/Lending.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Lending.module.css';
+
+const Lending = () => {
+    return (
+        <div>
+            Лендинг
+        </div>
+    );
+}
+
+export default Lending;
