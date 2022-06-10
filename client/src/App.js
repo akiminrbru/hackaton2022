9ed49a035703a1481a9812f3454f34a3bb695556
ArkoniX21 <chernovsergey21@yandex.ru>
2022-06-10 22:08:59 +0300

    Revert "1"
    This reverts commit 7ed65523cf6daa4cf99de9fdebbc6f94d77cf35f.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import Login from './Components/Login/Login';
-import Profile from './Components/Profile/Profile';
 import Register from './Components/Register/Register';
 
 function App() {
     return (
         <div className="App">
-            <Profile />
+            <Login />
         </div>
     );
 }
