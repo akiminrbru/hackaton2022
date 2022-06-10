1aa82610d7947789db7c6e9e548f41aba9a8878f
kiskisnotdead <72512459+kiskisnotdead@users.noreply.github.com>
2022-06-10 21:22:09 +0300

    login and register

---
+++ client/src/Components/Login/Login.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from './Login.module.css';
+import logo from './../../img/logo.svg';
+import logowhite from './../../img/logo2.svg';
+
+const Login = () => {
+    return (
+        <div>
+            <header className={styles.header}>
+                <div className="container">
+                    <div className={styles.header__content}>
+                        <div className={styles.logo}>
+                            <img className={styles.logo__img} src={logo} alt="logo"></img>
+                            <h1 className={styles.logo__h1}>Помогай</h1>
+                        </div>
+                        <nav className={styles.nav}>
+                            <div>
+                                <a className={styles.nav__auth} href="#">Вход</a>
+                            </div>
+                            <div className={styles.nav__box}>
+                                <a className={styles.nav__reg} href="#">Регистрация</a>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </header>
+            <article className={styles.main}>
+                <div className="container">
+                    <div className={styles.main__content}>
+                        <div className={styles.main__register}>
+                            <h2 className={styles.main__h2}>Вход</h2>
+                            <div className={styles.main__vvod}>
+                                <input placeholder="Email" className={styles.main__input}></input>
+                                <input placeholder="Пароль" className={styles.main__input}></input>
+                                <button className={styles.main__btn}>Войти</button>
+                                <p className={styles.main__p}>Нет аккаунта? <a href="#">Создайте.</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <footer className={styles.footer}>
+                <div className="container">
+                    <div className={styles.footer__logo}>
+                        <img src={logowhite} alt="logowhite"></img>
+                        <h2 className={styles.footer__h2}>Помогай</h2>
+                    </div>  
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Login;
