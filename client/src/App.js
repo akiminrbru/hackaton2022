dbe5bfe894260013244d9e953f1e0eb7a83eb5a4
Oleg Akimin <akimin97@mail.ru>
2022-06-10 21:58:31 +0300

    add nav

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,22 @@ import React from 'react';
 import './App.css';
 import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
+import Lending from './Components/Lending/Lending';
+import Profile from './Components/Profile/Profile';
+import {
+    Routes,
+    Route,
+} from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-            <Login/>
+            <Routes>
+                <Route path="/" element={<Lending />}/>
+                <Route path="/register" element={<Register />}/>
+                <Route path="/login" element={<Login />}/>
+                <Route path="/profile" element={<Profile />}/>
+            </Routes>
         </div>
     );
 }
---
+++ client/src/Components/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Profile.module.css';
+
+const Profile = () => {
+    return (
+        <div>
+            Профиль
+        </div>
+    );
+}
+
+export default Profile;
